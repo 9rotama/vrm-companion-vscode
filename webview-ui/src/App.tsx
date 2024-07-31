001ec11fccfbecfcfab43c4852184bdc5cf890f1
9rotama <aoim99.sc@gmail.com>
2024-08-01 00:34:58 +0900

    feat: usegltf使ってみる

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,54 +1,29 @@
 import { vscode } from "./utilities/vscode";
 import "./App.css";
-import { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+import { Suspense } from "react";
 
-function Box() {
-  // This reference will give us direct access to the mesh
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
+function Model() {
+  const gltf = useGLTF("/test_models/VRM_NekoUmiushi.vrm");
 
-  // Return view, these are regular three.js elements expressed in JSX
   return (
-    <mesh
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-      rotation={[45, 45, 45]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    <mesh rotation={[0, Math.PI, 0]} scale={[5, 5, 5]} position={[0, -3, 0]}>
+      <primitive object={gltf.scene} />
     </mesh>
   );
 }
 
 function App() {
-  function handleHowdyClick() {
-    vscode.postMessage({
-      command: "hello",
-      text: "Hey there partner! 🤠",
-    });
-  }
-
   return (
-    <main>
-      <h1>Hello World!</h1>
-      <button className="bg-slate-100 font-bold" onClick={handleHowdyClick}>
-        Howdy!
-      </button>
+    <main className="w-screen h-screen">
+      <div className="font-bold text-pink-600">vrm companion test</div>
       <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight
-          position={[10, 10, 10]}
-          angle={0.15}
-          penumbra={1}
-          decay={0}
-          intensity={Math.PI}
-        />
+        <ambientLight intensity={5} />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Box />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
       </Canvas>
     </main>
   );
