aa930fd2337345787439755af0d20de57812d189
9rotama <aoim99.sc@gmail.com>
2024-12-11 01:55:12 +0900

    chore: webview外を再構築(command登録、webview接続なし)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,28 @@
-// @ts-check
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-import eslint from "@eslint/js";
-import tsEslint from "typescript-eslint";
+export default [{
+    files: ["**/*.ts"],
+}, {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
 
-export default tsEslint.config(eslint.configs.recommended, tsEslint.configs.recommended);
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 2022,
+        sourceType: "module",
+    },
+
+    rules: {
+        "@typescript-eslint/naming-convention": ["warn", {
+            selector: "import",
+            format: ["camelCase", "PascalCase"],
+        }],
+
+        curly: "warn",
+        eqeqeq: "warn",
+        "no-throw-literal": "warn",
+        semi: "warn",
+    },
+}];
