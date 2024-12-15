c7e2c846643d82cc40e6edaabc0df097d2bf13dd
9rotama <aoim99.sc@gmail.com>
2024-12-15 23:23:11 +0900

    feat: display vite output on panel

--- src/webview-provider.ts
+++ src/webview-provider.ts
@@ -1,14 +1,11 @@
 import * as vscode from "vscode";
+import { Uri, Webview } from "vscode";
 
 export class WebViewProvider implements vscode.WebviewViewProvider {
   constructor(private _extensionUri: vscode.Uri) {}
   private _view?: vscode.WebviewView;
 
-  public resolveWebviewView(
-    webviewView: vscode.WebviewView,
-    context: vscode.WebviewViewResolveContext,
-    token: vscode.CancellationToken
-  ) {
+  public resolveWebviewView(webviewView: vscode.WebviewView) {
     this._view = webviewView;
 
     webviewView.webview.options = {
@@ -16,18 +13,61 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
       enableScripts: true,
       localResourceRoots: [this._extensionUri],
     };
-    webviewView.webview.html = `
+    webviewView.webview.html = this._getHtml(
+      this._view.webview,
+      this._extensionUri
+    );
+  }
+
+  private _getHtml(webview: Webview, extensionUri: Uri) {
+    // The CSS file from the React build output
+    const stylesUri = getUri(webview, extensionUri, [
+      "packages",
+      "webview",
+      "build",
+      "assets",
+      "index.css",
+    ]);
+    // The JS file from the React build output
+    const scriptUri = getUri(webview, extensionUri, [
+      "packages",
+      "webview",
+      "build",
+      "assets",
+      "index.js",
+    ]);
+
+    const nonce = getNonce();
+
+    return /*html*/ `
       <!DOCTYPE html>
-      <html lang="ja">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>WebView Example</title>
-      </head>
-      <body>
-        <h1>Hello World!</h1>
-      </body>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+          <link rel="stylesheet" type="text/css" href="${stylesUri}">
+          <title>Hello World</title>
+        </head>
+        <body>
+          <div id="root"></div>
+          <script type="module" nonce="${nonce}" src="${scriptUri}"></script>
+        </body>
       </html>
     `;
   }
+}
+
+function getNonce() {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+function getUri(webview: Webview, extensionUri: Uri, pathList: string[]) {
+  return webview.asWebviewUri(Uri.joinPath(extensionUri, ...pathList));
 }
