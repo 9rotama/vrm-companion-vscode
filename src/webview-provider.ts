3878e7916da43d4a25cc0d0b16eebf84dffe18df
9rotama <aoim99.sc@gmail.com>
2024-12-15 22:20:32 +0900

    feat: setup webview view on explorer

---
+++ src/webview-provider.ts
@@ -0,0 +1,33 @@
+import * as vscode from "vscode";
+
+export class WebViewProvider implements vscode.WebviewViewProvider {
+  constructor(private _extensionUri: vscode.Uri) {}
+  private _view?: vscode.WebviewView;
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    token: vscode.CancellationToken
+  ) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+      localResourceRoots: [this._extensionUri],
+    };
+    webviewView.webview.html = `
+      <!DOCTYPE html>
+      <html lang="ja">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>WebView Example</title>
+      </head>
+      <body>
+        <h1>Hello World!</h1>
+      </body>
+      </html>
+    `;
+  }
+}
