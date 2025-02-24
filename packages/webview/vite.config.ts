4dbf3d206d2e03cec0488b797baecd41988fda4a
9rotama <aoim99.sc@gmail.com>
2025-02-24 12:12:23 +0900

    chore: init vite `packages/webview`

--- packages/webview/vite.config.ts
+++ packages/webview/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build",
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()],
 });
